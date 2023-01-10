cb591532ce7d832bab1e522bc55d4330fc080128
HungryStudent <nikita.efan.2016@mail.ru>
2023-01-10 22:49:10 +0300

    Наверное фикс options, я не знаю

--- lkb/main.py
+++ lkb/main.py
@@ -7,19 +7,9 @@ from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI(docs_url="/api", redoc_url="/api/redoca", openapi_url="/api/openapi.json")
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"]
 )
 
 app.include_router(users.router, prefix="/api/user")
