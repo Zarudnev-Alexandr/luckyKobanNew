d825deebd02ec2edc67644a2328ffea87f71e5fd
HungryStudent <nikita.efan.2016@mail.ru>
2023-01-16 21:16:05 +0300

    Заменил КУКИ на ЗАГОЛОВКИ. Как саша и просил

--- lkb/core/jwt_funcs.py
+++ lkb/core/jwt_funcs.py
@@ -1,13 +1,13 @@
 import jwt
-from fastapi import Cookie, HTTPException
+from fastapi import Header, HTTPException
 from jwt import DecodeError
 
 from configs import salt
 
 
-def get_info_token(token: str = Cookie()):
+def get_info_token(authorization: str = Header()):
     try:
-        user_data = jwt.decode(token, salt, algorithms=["HS256"])
+        user_data = jwt.decode(authorization, salt, algorithms=["HS256"])
     except DecodeError:
         raise HTTPException(401, "invalid token")
     return user_data["user_id"]
